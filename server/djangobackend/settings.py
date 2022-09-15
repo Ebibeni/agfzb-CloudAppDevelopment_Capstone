5bc8f3dda20f2dcf68d46caccca40a2fd4e93c8a
“Ebibeni” <ebibeni.dbig@gmail.com>
2022-09-15 08:41:40 -0400

    changes made to the settings.py for route

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ['localhost,capstoneebibeni.us-south.cf.appdomain.cloud/']
+ALLOWED_HOSTS = ["capstoneebibeni.us-south.cf.appdomain.cloud",".localhost", '127.0.0.1']
 
 
 # Application definition
