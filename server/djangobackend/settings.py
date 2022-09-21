6b71deb7dfdb99c361b4219b785c0f35404da9a1
“Ebibeni” <ebibeni.dbig@gmail.com>
2022-09-21 09:30:51 -0400

    correct changes made to manifest

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ['capstoneebibenifinal.us-south.cf.appdomain.cloud','localhost']
+ALLOWED_HOSTS = ['ebibenicapstone.mybluemix.net','localhost']
 
 
 # Application definition
